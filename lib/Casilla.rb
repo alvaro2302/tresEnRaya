c112db563e047fcf2934683c1e86ba6401d909b2
alvaro2302 <alvaro230297@hotmail.com>
2018-11-14 11:47:22 -0400

    add test pasando

--- lib/Casilla.rb
+++ lib/Casilla.rb
@@ -6,11 +6,20 @@ class Casilla
         @marcado = false
     end
     def marcar(fila,columna)
-       @marcado = true
+
+       @marcado = esValidaLaPosicion(fila,columna)
     end
 
     def marcado()
         return @marcado
+    end
+
+    def esValidaLaPosicion(fila,columna)
+        esValida = false
+        if fila<=2 and columna<=2
+            esValida = true
+        end
+        return esValida
     end
 
     
